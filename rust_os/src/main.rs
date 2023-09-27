b16d2d19cd0f657b90c11c70a0c13095a07d95eb
许菡如 <elina@xuhanrudeMacBook.local>
2023-09-26 22:03:08 -0400

    keep main branch clean

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -1,11 +1,3 @@
 fn main() {
-    let x = 4;
     println!("Hello, world!");
-    println!("x is: {}", x);
-    let x = 2 * x;
-    println!("x is: {}", x);
-    const SECONDS_IN_MINUTE: u32 = 60;
-    println!("x is: {}", SECONDS_IN_MINUTE);
-    let boolean: bool = true;
-    println!("bool is {}", boolean);
 }
