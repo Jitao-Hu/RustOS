c03991e2566117c45f0e6eeb58338a6398a3a907
Jerry Hu <jitaohu88@gmail.com>
2023-11-08 23:39:28 -0500

    use build-std feature to recompile core, rustc-std

---
+++ rust_os/target/x86_64-rust_os/debug/deps/rust_os-79c57f4d7081d660.d
@@ -0,0 +1,5 @@
+/Users/hujitao/Desktop/RustOS/rust_os/target/x86_64-rust_os/debug/deps/rust_os-79c57f4d7081d660: src/main.rs
+
+/Users/hujitao/Desktop/RustOS/rust_os/target/x86_64-rust_os/debug/deps/rust_os-79c57f4d7081d660.d: src/main.rs
+
+src/main.rs:
---
+++ rust_os/target/x86_64-rust_os/debug/deps/rust_os-7cdb39027e6281d3.d
@@ -0,0 +1,5 @@
+/Users/hujitao/Desktop/RustOS/rust_os/target/x86_64-rust_os/debug/deps/rust_os-7cdb39027e6281d3: src/main.rs
+
+/Users/hujitao/Desktop/RustOS/rust_os/target/x86_64-rust_os/debug/deps/rust_os-7cdb39027e6281d3.d: src/main.rs
+
+src/main.rs:
